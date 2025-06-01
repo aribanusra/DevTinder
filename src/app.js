a477b7f01bcae1f9001e353d0fd312c2997f1fd8
Ariba Nusra <ariba.officialnusra@gmail.com>
2025-06-01 11:55:05 +0530

    server created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("starting a new project")
+const express = require ('express')
+const app = express();
+app.use('/', (req,res)=>{
+res.send("hello from dashboard")
+})
+app.listen (7777 , ()=>{
+console.log("port 7777")
+})
